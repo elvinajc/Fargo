e1f95e8f9931055ff4bea37c573f11945ff621ac
elvinajc <elvina14jc@gmail.com>
2022-07-20 09:11:46 +0700

    Initial Commit

---
+++ Fargo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fargo
+//
+//  Created by Elvina Jacia on 20/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
