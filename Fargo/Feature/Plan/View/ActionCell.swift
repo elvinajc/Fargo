dcd43684a75949160d2d03428aa4195a5592cbd3
elvinajc <elvina14jc@gmail.com>
2022-07-27 11:12:25 +0700

    checkmark button status saved on core data

--- Fargo/Feature/Plan/View/ActionCell.swift
+++ Fargo/Feature/Plan/View/ActionCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class ActionCell: UITableViewCell {
     
@@ -19,17 +20,20 @@ class ActionCell: UITableViewCell {
      let uncheckIcon = UIImage(systemName: "square")
      let checkIcon = UIImage(systemName: "checkmark.square.fill")
     
+     var actionStatus = ""
+    
+    //Core Data Properties
+    var actionplans = [ActionPlan]()
+    var firstLoad = true
+    let context  = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-         checkmarkButton.tintColor = .softGray
-//        if checkmarkButton.currentImage == uncheckIcon{
-//            //checkmarkButton.tintColor = .softGray
-//            checkmarkButton.backgroundColor = .clear
-//        } else {
-//            checkmarkButton.tintColor = .darkChoco
-//        }
+        fetchStatus()
+    
+      
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -47,15 +51,83 @@ class ActionCell: UITableViewCell {
         
       
             //checkmarkButton.isSelected = false
+        
+        if checkmarkButton.currentImage == uncheckIcon{
             checkmarkButton.setImage(checkIcon, for: .normal)
             checkmarkButton.tintColor = .darkChoco
-       
             checkmarkButton.isSelected = true
-    
-        
-        print("CHECKMARK DIPENCET")
-        
+            actionStatus = "Done"
+            
+            updateCheckmarkStatus()
+            print("CHECKMARK Done")
+            
+        } else {
+            checkmarkButton.setImage(uncheckIcon, for: .normal)
+            checkmarkButton.tintColor = .softGray
+            checkmarkButton.isSelected = true
+            actionStatus = "Undone"
+            
+            updateCheckmarkStatus()
+            print("CHECKMARK Undone")
+        }
         
     }
+    
+    func updateCheckmarkStatus(){
+    
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "ActionPlan")
+        do {
+            let results: NSArray = try context.fetch(request) as NSArray
+            for result in results{
+                let actPlan = result as! ActionPlan
+                actPlan.status = actionStatus
+                    
+                    try context.save()
+
+                    }
+        } catch  {
+                print("Fetch failed")
+            }
+    }
+
+    
+    func fetchStatus(){
+        //Fetech statusnya
+        let request = NSFetchRequest<NSFetchRequestResult> (entityName: "ActionPlan")
+        
+        do{
+            
+            let results : NSArray = try context.fetch(request) as NSArray
+            let pred = NSPredicate(format: "status == %@", "Undone")
+            request.predicate = pred
+            
+            for result in results {
+                let act = result as! ActionPlan
+                
+                //let actStatus = (result as AnyObject).value(forKey: "status") as? String
+                actionplans.append(act)
+                
+                if act.status == "Done"{
+                    checkmarkButton.setImage(checkIcon, for: .normal)
+                    checkmarkButton.tintColor = .darkChoco
+                    checkmarkButton.isSelected = true
+                } else{
+                    checkmarkButton.setImage(uncheckIcon, for: .normal)
+                    checkmarkButton.tintColor = .softGray
+                    checkmarkButton.isSelected = false
+                }
+
+            }
+        }catch{
+            print(error)
+        }
+    }
+    
+    func saveStatus(){
+        
+    }
+    
+    
+    
     
 }
